d7bd9860d199cdb0adc5fa4ab0484882216b5601
hirdeshmewada <djhd221@gmail.com>
2024-05-15 20:45:45 +0530

    day 14

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -1,2 +1,20 @@
-https://www.naukri.com/code360/problem-details/insertion-sort_3155179
+// https://www.naukri.com/code360/problem-details/insertion-sort_3155179
 
+// #include <bits/stdc++.h> 
+// void insertionSort(int n, vector<int> &arr){
+
+//   for (int i = 1; i < n; i++) {   
+//   int temp=arr[i];
+//   int j=i-1;
+//   for(;j>=0;j--){
+//     if (arr[j] > temp) {
+//      //shift
+//      arr[j+1]=arr[j];
+//     } else {
+//      // ruk jao
+//      break;
+//     }
+//   }
+//   arr[j+1]=temp;
+//   }
+// }
